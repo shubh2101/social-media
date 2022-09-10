7a75a68afe8564e0b54f6d54f473ee9bf6e026b3
Shubham Singh <shubham2101@gmail.com>
2022-09-11 02:06:04 +0530

    NavBar

---
+++ src/assets/icons.js
@@ -0,0 +1,6 @@
+import MailIcon from "@mui/icons-material/Mail";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+import Diversity1Icon from "@mui/icons-material/Diversity1";
+import Avatar from '@mui/material/Avatar';
+
+export { MailIcon, NotificationsIcon, Diversity1Icon , Avatar};
---
+++ src/components/NavBar.js
@@ -0,0 +1,51 @@
+import {
+  MailIcon,
+  Diversity1Icon,
+  NotificationsIcon,
+  Avatar,
+} from "../assets/icons";
+import { AppBar, Badge, Box, styled, Toolbar, Typography } from "@mui/material";
+
+const Icons = styled(Box)(() => ({
+  alignItems: "center",
+  gap: "20px",
+  display: "flex",
+}));
+
+const StyledToolbar = styled(Toolbar)({
+  display: "flex",
+  justifyContent: "space-between",
+});
+
+const NavBar = () => {
+  return (
+    <AppBar position="sticky">
+      <StyledToolbar>
+        <Badge>
+          <Diversity1Icon />
+          <Typography
+            variant="h6"
+            marginX="20px"
+            sx={{ display: { xs: "none", sm: "block" } }}
+          >
+            La familia
+          </Typography>
+        </Badge>
+
+        <Icons>
+          <Badge badgeContent={4} color="error">
+            <MailIcon />
+          </Badge>
+          <Badge badgeContent={17} color="error">
+            <NotificationsIcon />
+          </Badge>
+          <Avatar
+            alt="Jose"
+            src="https://editorial.uefa.com/resources/01de-0e7311a0c694-24ea806e4996-1000/format/wide1/jose_mourinho_wants_his_inter_side_to_follow_on_from_their_weekend_victory.jpeg?imwidth=2048"
+          />
+        </Icons>
+      </StyledToolbar>
+    </AppBar>
+  );
+};
+export default NavBar;
