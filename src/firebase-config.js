da7ec33a0cba86979be3e8eb8ccef4948ce043dc
Shubham Singh <shubham2101@gmail.com>
2022-09-18 21:11:09 +0530

    setting up firebase config

---
+++ src/firebase-config.js
@@ -0,0 +1,23 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const apiKey = process.env.REACT_APP_apiKey;
+const authDomain = process.env.REACT_APP_authDomain;
+const projectId = process.env.REACT_APP_projectId;
+const storageBucket = process.env.REACT_APP_storageBucket;
+const messagingSenderId = process.env.REACT_APP_messagingSenderId;
+const appId = process.env.REACT_APP_appId;
+const measurementId = process.env.REACT_APP_measurementId;
+
+const firebaseConfig = {
+  apiKey,
+  authDomain,
+  projectId,
+  storageBucket,
+  messagingSenderId,
+  appId,
+  measurementId,
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
