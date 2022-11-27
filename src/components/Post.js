9304806b0611600971dd0a9848f69bad69d7852e
Shubham Singh <shubham2101@gmail.com>
2022-11-28 03:03:32 +0530

    more accurate timestamp of posts

--- src/components/Post.js
+++ src/components/Post.js
@@ -22,7 +22,16 @@ const Post = ({ post }) => {
     const daysPassed = Math.round(
       Math.abs((new Date() - postDate) / (1000 * 24 * 3600))
     );
-    if (daysPassed === 0) return "Today";
+    const hourPassed = Math.round(
+      Math.abs((new Date() - postDate) / (1000 * 3600))
+    );
+    const minsPassed = Math.round(
+      Math.abs((new Date() - postDate) / (1000 * 60))
+    );
+
+    if (minsPassed === 0) return "Just now";
+    if (minsPassed < 60) return `${minsPassed} mins ago`;
+    if (hourPassed < 24) return `${hourPassed} hr ago`;
     if (daysPassed === 1) return "Yesterday";
     if (daysPassed <= 7) return `${daysPassed} days ago`;
     const day = `${postDate.getDate()}`.padStart(2, 0);
