41609efd01f21be6f5c3604af9c4764cbe128deb
Shubham Singh <shubham2101@gmail.com>
2023-02-26 01:38:03 +0530

    search results for search-query and No users found

--- src/components/Explore.js
+++ src/components/Explore.js
@@ -14,29 +14,59 @@ const Explore = () => {
   const searchQuery = searchParams.get("filter");
   const newFilterUsers = otherUsers.filter((user) => {
     const name = `${user.data.firstname} ${user.data.lastname}`;
-    return searchQuery ? name.toLowerCase().includes(searchQuery) : true;
+    return searchQuery
+      ? name.toLowerCase().includes(searchQuery.toLowerCase())
+      : true;
   });
+
   return (
     <Box flex={2} p={2} mt={4}>
       <Card
         sx={{ maxWidth: 500, p: 1, borderRadius: "10px", mt: 4 }}
         elevation={8}
       >
-        <Box display="flex" justifyContent="center">
+        <Box
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+          flexDirection="column"
+          gap={2}
+        >
+          {newFilterUsers.length !== 0 && (
+            <Typography
+              variant="h6"
+              sx={{
+                fontWeight: "600",
+                letterSpacing: ".25rem",
+              }}
+            >
+              People
+            </Typography>
+          )}
+          {searchQuery && (
+            <Typography variant="body">
+              Search results for "{searchQuery}"
+            </Typography>
+          )}
+        </Box>
+        {newFilterUsers.length ? (
+          newFilterUsers.map((user) => (
+            <FollowUser user={user.data} key={user.data.userId} />
+          ))
+        ) : (
           <Typography
             variant="h6"
+            p={2}
             sx={{
+              justifyContent: "center",
+              display: "flex",
               fontWeight: "600",
               letterSpacing: ".25rem",
             }}
           >
-            People
+            No users Found
           </Typography>
-        </Box>
-
-        {newFilterUsers.map((user) => (
-          <FollowUser user={user.data} key={user.data.userId} />
-        ))}
+        )}
       </Card>
     </Box>
   );
